d173f0b2d8dfc47d7983a2dcd64db28d17a52bd5
apisit <apisitjadi@hotmail.com>
2015-03-26 00:08:55 +0700

    add gulp 02

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('browser-sync', function() {
+	var config = {
+		server: {
+			baseDir: "./"
+		},
+		port: 8899
+	};
+	browserSync(config);
+});
+
+gulp.task('default', ['browser-sync'],function(){
+	gulp.watch(['*.html'], browserSync.reload);
+});
